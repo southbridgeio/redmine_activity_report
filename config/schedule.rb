577b9564a509d6fc9d28200158bc8b00fe52513b
Artur Trofimov <arturtr@gmail.com>
2015-12-21 17:05:54 +1000

    Fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
-every :day do
+every :day, at: '0:03' do
   rake 'activity_report:daily'
 end
 
-every :monday do
+every :monday, at: '0:07' do
   rake 'activity_report:weekly'
 end
 
-every :month do
+every :month, at: '0:11' do
   rake 'activity_report:monthly'
 end
