ab1da8e4259faeaee170d619d769aba06d8d9436
Artur Trofimov <arturtr@gmail.com>
2016-03-04 16:35:25 +1000

    Update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
-every :day, at: '0:03' do
+every :day, at: '6:03' do
   rake 'activity_report:daily'
 end
 
-every :monday, at: '0:07' do
+every :monday, at: '6:07' do
   rake 'activity_report:weekly'
 end
 
-every :month, at: '0:11' do
+every :month, at: '6:11' do
   rake 'activity_report:monthly'
 end
