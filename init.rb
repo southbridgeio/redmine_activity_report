2f4536d7baab7b2e338aad9c5ad514cd66b0daac
Artur Trofimov <arturtr@gmail.com>
2016-03-17 11:37:05 +1000

    Add link to project in report tables, fix project column width

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_activity_report do
   name 'Redmine Activity Report plugin'
   url 'https://github.com/centosadmin/redmine_activity_report'
   description 'This is a plugin for Redmine which generate time reports for projects'
-  version '1.2.1'
+  version '1.2.2'
   author 'Centos-admin.ru'
   author_url 'http://centos-admin.ru'
 
