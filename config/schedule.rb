589f7f6357411b255a2104475d4cd9b296f53dac
Steven.W <archonwang1981@gmail.com>
2016-12-26 13:39:21 +0800

    add

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,14 @@
+# start everyday on 6:03 am to generate daily report
 every :day, at: '6:03' do
   rake 'activity_report:daily'
 end
 
+# start Monday on 6:07 am to generate weekly report
 every :monday, at: '6:07' do
   rake 'activity_report:weekly'
 end
 
+# start 1st day of month on 6:11 am to generate monthly report
 every :month, at: '6:11' do
   rake 'activity_report:monthly'
 end
