ec14f0e81ff8cb5a9d7422676f863b0c22043576
Igor Olemskoi <igor@southbridge.ru>
2019-11-19 23:10:28 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -5,11 +5,11 @@ require_dependency 'activity_report/patches/projects_helper_patch'
 
 Redmine::Plugin.register :redmine_activity_report do
   name 'Redmine Activity Report plugin'
-  url 'https://github.com/centosadmin/redmine_activity_report'
+  url 'https://github.com/southbridgeio/redmine_activity_report'
   description 'This is a plugin for Redmine which generate time reports for projects'
   version '1.2.10'
   author 'Southbridge'
-  author_url 'https://github.com/centosadmin'
+  author_url 'https://github.com/southbridgeio'
 
   settings(partial: 'activity_report/settings',
            default: { 'time_for_reaction' => 15,
